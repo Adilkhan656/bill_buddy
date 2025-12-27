75f3217e393016cb3731fc764ff53209c88edf0a
adilkhan <aaadilkhan656@gmail.com>
2025-12-27 19:17:34 +0530

    firebase integration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,29 +1,33 @@
+// android/app/build.gradle.kts
+
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // Use the modern Kotlin plugin ID
+    id("org.jetbrains.kotlin.android") 
+    // Flutter plugin
     id("dev.flutter.flutter-gradle-plugin")
+    // FIREBASE (Apply it here without version)
+    id("com.google.gms.google-services") 
 }
 
 android {
-    namespace = "com.example.bill_buddy"
+    namespace = "com.example.bill_buddy" // Your Package Name
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // Ensure this matches your Firebase Console package name exactly
         applicationId = "com.example.bill_buddy"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,13 +36,17 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
-flutter {
-    source = "../.."
+dependencies {
+    // Import the Firebase BoM (Bill of Materials)
+    // I corrected your version "34.7.0" (which doesn't exist) to a valid recent one "33.7.0"
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    
+    // Add the dependencies for the Firebase products you want to use
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth") 
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+// android/build.gradle.kts
+
 allprojects {
     repositories {
         google()
@@ -5,10 +7,7 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
@@ -21,4 +20,16 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// --- CORRECTED PLUGINS BLOCK ---
+plugins {
+    // 1. We REMOVED "com.android.application" and "com.android.library"
+    //    because Flutter is already loading them for you.
+
+    // 2. Keep Kotlin (This is usually safe to define)
+   
+
+    // 3. Keep Google Services (This is what we need for Firebase)
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
